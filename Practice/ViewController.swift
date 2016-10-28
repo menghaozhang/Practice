74cac0a3168cc36f4c61672f2ad89212dd8a84e6
menghaozhang <menghao.zhang@ext.accedo.tv>
2016-10-28 19:43:32 +0200

    Init

---
+++ Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by paul on 28/10/16.
+//  Copyright © 2016 Menghao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
